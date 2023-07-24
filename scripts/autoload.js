476e9ce064a771a52f6c9d73181f52bce945388a
andika wahyudi <andikawpb200@gmail.com>
2023-07-25 00:29:57 +0700

    fix and add resume

--- scripts/autoload.js
+++ scripts/autoload.js
@@ -5,4 +5,11 @@ document.write('<script src="/scripts/bigtitle.js"></script>');
 window.onload = () => {
   const title = document.getElementsByClassName("big-title")[0];
   glitchedText(title);
+
+  const sidebarMenus = Array.from(
+    document.getElementsByClassName("sidebar-menu-item")
+  );
+  sidebarMenus.forEach((element, index) => {
+    element.onclick = () => toggleSidebarOff();
+  });
 };
